11a4d6ec22210591b983b05c5350dd49dbfc1cfc
George Bruner <gbruner@gmail.com>
2017-01-25 11:50:47 -0500

    add some more tests

--- addition.rb
+++ addition.rb
@@ -1,3 +1,7 @@
 def add(num1,num2)
+	result = num1 + num2
+	
 end
+
+
 
--- test_addition.rb
+++ test_addition.rb
@@ -3,10 +3,20 @@ require_relative "addition.rb"
 
 class Test_Addition < Minitest::Test
 
-#add two numbers together
+#1+1=2
 	def test_1_plus_1_returns_2
 		assert_equal(2,add(1,1))
 
 	end
+#2+3 = 5
+	def test_2_plus_3_returns_5
+		assert_equal(5,add(2,3))
+	end
 
+#-4+10=6	
+    def test_neg4_plus10_returns6
+		assert_equal(6,add(-4,10))
+	end
+	
 end
+
